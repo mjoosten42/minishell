4162c29cec9eb23f09df61f996fa34052d19de7b
RubenNijhuis <contact@rubennijhuis.com>
2022-03-21 13:58:39 +0100

    feat: lexer parsing string with special chars [WIP]

--- src/lexer.c
+++ src/lexer.c
@@ -20,14 +20,20 @@
 void	token_add_back(t_token **head, t_token *new_token)
 {
 	t_token	*token;
+	int		i;
 
+	i = 1;
 	token = *head;
 	if (token)
 	{
 		while (token->next)
+		{
+			i++;
 			token = token->next;
+		}
 		token->next = new_token;
 		new_token->prev = token;
+		new_token->position = i;
 	}
 	else
 		*head = new_token;
@@ -36,10 +42,9 @@ void	token_add_back(t_token **head, t_token *new_token)
 t_token	*token_new(void)
 {
 	t_token		*token;
-	static int	i;
 
 	token = ft_malloc(sizeof(t_token));
-	token->position = i++;
+	token->position = 0;
 	token->prev = NULL;
 	token->next = NULL;
 	return (token);
@@ -51,10 +56,8 @@ char	*get_token_value(char *str)
 	int		i;
 
 	i = 0;
-	printf("Str: %s\n", str);
-	while (ft_strchr(WORD_END, str[i]))
+	while (!ft_strchr(WORD_END, str[i]))
 		i++;
-	printf("Str: %s\nI: %d\n", str, i);
 	value = ft_malloc(i + 1);
 	ft_strlcpy(value, str, i + 1);
 	return (value);
@@ -66,15 +69,17 @@ t_token	*word_token(char *str)
 
 	token = token_new();
 	token->value = get_token_value(str);
-	printf("%s - token val\n", token->value);
 	return (token);
 }
 
 t_token	*special_char_token(char *str)
 {
 	t_token	*token;
+	char	*new;
 	char	c;
+	int		len;
 
+	len = 0;
 	c = *str;
 	token = token_new();
 	if (c == '\'')
@@ -89,13 +94,17 @@ t_token	*special_char_token(char *str)
 		token->type = pipe_char;
 	if (c == '$')
 		token->type = dollar;
-	token->value = get_token_value(str);
+	while (ft_strchr(SPECIAL_CHARS, str[len]))
+		len++;
+	new = ft_malloc(len + 1);
+	ft_strlcpy(new, str, len + 1);
+	token->value = new;
 	return (token);
 }
 
 void	print_tokens(t_token *token)
 {
-	printf("- id - type - value -\n");
+	printf("\n- id - type - value -\n");
 	while (token)
 	{
 		printf("|  %2i |  %i   | %s\n", token->position, token->type, token->value);
@@ -106,7 +115,6 @@ void	print_tokens(t_token *token)
 void	lexer(char *str)
 {
 	t_token	*head;
-	char	*start;
 	int		i;
 	int		in_word;
 
@@ -117,17 +125,13 @@ void	lexer(char *str)
 	{
 		if (in_word == 0 && ft_isalpha(str[i]))
 		{
-			printf("In word %c\n", str[i]);
-			start = str + i;
+			token_add_back(&head, word_token(str + i));
 			in_word = 1;
 		}
-		//if (ft_strchr(SPECIAL_CHARS, str[i]))
-		//	token_add_back(&head, special_char_token(str + i));
 		if (in_word == 1 && !ft_isalpha(str[i]))
-		{
-			token_add_back(&head, word_token(start));
 			in_word = 0;
-		}
+		if (ft_strchr(SPECIAL_CHARS, str[i]))
+			token_add_back(&head, special_char_token(str + i));
 		i++;
 	}
 	print_tokens(head);
