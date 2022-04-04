2a1a8244a504170e39f09fdea25ba10df9b31748
Maarten Joosten <mjoosten@student.codam.nl>
2022-04-04 16:40:28 +0200

    Made ft_init

--- src/main.c
+++ src/main.c
@@ -5,21 +5,15 @@
 
 t_program_data	g_pd;
 
-void	copy_env(void);
+void	ft_init(void);
 void	ft_signal(int signum);
 
 int	main(void)
 {
-	extern int	rl_catch_signals;
 	t_token		*head;
 	char		*str;
 
-	copy_env();
-	rl_catch_signals = 0;
-	//signal(SIGINT, ft_signal);
-	signal(SIGQUIT, ft_signal);
-	signal(SIGCHLD, ft_signal);
-	g_pd.dir = getcwd(g_pd.dir, 0);
+	ft_init();
 	while (1)
 	{
 		head = token_start();
@@ -29,16 +23,17 @@ int	main(void)
 		if (*str)
 			add_history(str);
 		lexer(head, str);
+		free(str);
 		ft_expand(head);
 		print_tokens(head);
 		ft_parse(head, STDIN_FILENO);
 		ft_remove_token(head);
-		free(str);
 	}
 }
 
-void	copy_env(void)
+void	ft_init(void)
 {
+	extern int	rl_catch_signals;
 	extern char	**environ;
 	int			i;
 
@@ -50,6 +45,15 @@ void	copy_env(void)
 	g_pd.env[i] = NULL;
 	while (i--)
 		g_pd.env[i] = ft_strdup(environ[i]);
+	rl_catch_signals = 0;
+	//signal(SIGINT, ft_signal);
+	signal(SIGQUIT, ft_signal);
+	signal(SIGCHLD, ft_signal);
+	g_pd.dir = getcwd(g_pd.dir, 0);
+}
+
+void	copy_env(void)
+{
 }
 
 void	ft_signal(int signum)
