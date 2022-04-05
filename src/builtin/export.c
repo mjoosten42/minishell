eea2be19cfb65a2d56fe4cd0412e00e305ce8b47
Maarten Joosten <mjoosten@student.codam.nl>
2022-04-05 10:53:47 +0200

    Removed variable from export

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -4,12 +4,11 @@
 void	export(char *variable)
 {
 	char	**new_env;
-	int		new_env_size;
 	int		i;
 
 	i = 0;
-	new_env_size = g_pd.amount_env_lines + 2;
-	new_env = ft_malloc(new_env_size * sizeof(char *));
+	g_pd.amount_env_lines++;
+	new_env = ft_malloc((g_pd.amount_env_lines + 1) * sizeof(char *));
 	while (g_pd.env[i])
 	{
 		new_env[i] = g_pd.env[i];
@@ -19,5 +18,4 @@ void	export(char *variable)
 	new_env[i + 1] = NULL;
 	free(g_pd.env);
 	g_pd.env = new_env;
-	g_pd.amount_env_lines++;
 }
