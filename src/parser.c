805ee56689a036418b9698a749db4846d44ae88d
Maarten Joosten <mjoosten@student.codam.nl>
2022-04-11 13:03:32 +0200

    Changed get_fds to not segfault, shouldn't have worked in the first place

--- src/parser.c
+++ src/parser.c
@@ -4,8 +4,8 @@
 
 char	**ft_get_args(t_token *token);
 int		ft_get_fds(t_token *token, int fds[2]);
-int		ft_redirect(t_token *token, int *fd, int flags);
 int		ft_get_pipe(t_token *token, int fds[2]);
+int		ft_redirect(t_token *token, int *fd, int flags);
 
 void	ft_parse(t_token *head, int pipefd)
 {
@@ -36,64 +36,6 @@ void	ft_parse(t_token *head, int pipefd)
 		wait(NULL);
 }
 
-int	ft_get_fds(t_token *token, int fds[2])
-{
-	t_type	type;
-	int		ret;
-
-	ret = 0;
-	token = token->next;
-	while (token)
-	{
-		type = token->type;
-		if (type == pipe_char)
-			ret = ft_get_pipe(token, fds);
-		if (type == here_doc)
-			ret = ft_here_doc(token, &fds[0]);
-		if (type == red_in)
-			ret = ft_redirect(token, &fds[0], O_RDONLY);
-		if (type == red_out)
-			ret = ft_redirect(token, &fds[1], O_WRONLY | O_CREAT);
-		if (type == red_out_app)
-			ret = ft_redirect(token, &fds[1], O_WRONLY | O_CREAT | O_APPEND);
-		if (ret)
-			return (ret);
-		token = token->next;
-	}
-	return (0);
-}
-
-int	ft_redirect(t_token *token, int *fd, int flags)
-{
-	token = token->prev;
-	ft_remove_token(token->next);
-	token = token->next;
-	if (*fd > STDERR_FILENO)
-		ft_close(*fd);
-	if (!token || token->type != word)
-		return (ft_return_error("Syntax error: expected redirect target"));
-	*fd = ft_open(token->value, flags, 0644);
-	ft_remove_token(token);
-	return (0);
-}
-
-int	ft_get_pipe(t_token *token, int fds[2])
-{
-	int	pipefds[2];
-
-	if (fds[1] > STDERR_FILENO)
-		ft_close(fds[1]);
-	if (token->prev->type != word || !token->next)
-	{
-		if (fds[0] > STDERR_FILENO)
-			ft_close(fds[0]);
-		return (ft_return_error("Syntax error: pipe missing commands"));
-	}
-	ft_pipe(pipefds);
-	fds[1] = pipefds[1];
-	return (pipefds[0]);
-}
-
 char	**ft_get_args(t_token *token)
 {
 	char	**strs;
@@ -114,4 +56,63 @@ char	**ft_get_args(t_token *token)
 		ft_remove_token(token->next);
 	}
 	return (strs);
+}
+
+int	ft_get_fds(t_token *token, int fds[2])
+{
+	t_token	*next;
+	t_type	type;
+	int		ret;
+
+	ret = 0;
+	while (token->next)
+	{
+		next = token->next;
+		type = next->type;
+		if (type == pipe_char)
+			ret = ft_get_pipe(next, fds);
+		if (type == here_doc)
+			ret = ft_here_doc(next, &fds[0]);
+		if (type == red_in)
+			ret = ft_redirect(next, &fds[0], O_RDONLY);
+		if (type == red_out)
+			ret = ft_redirect(next, &fds[1], O_WRONLY | O_CREAT);
+		if (type == red_out_app)
+			ret = ft_redirect(next, &fds[1], O_WRONLY | O_CREAT | O_APPEND);
+		if (ret)
+			return (ret);
+		token = token->next;
+	}
+	return (0);
+}
+
+int	ft_get_pipe(t_token *token, int fds[2])
+{
+	int	pipefds[2];
+
+	if (fds[1] > STDERR_FILENO)
+		ft_close(fds[1]);
+	if (token->prev->type != word || !token->next)
+	{
+		if (fds[0] > STDERR_FILENO)
+			ft_close(fds[0]);
+		return (ft_return_error("Syntax error: pipe missing commands"));
+	}
+	ft_pipe(pipefds);
+	fds[1] = pipefds[1];
+	return (pipefds[0]);
+}
+
+int	ft_redirect(t_token *token, int *fd, int flags)
+{
+	token = token->prev;
+	ft_remove_token(token->next);
+	token = token->next;
+	if (*fd > STDERR_FILENO)
+		ft_close(*fd);
+	if (!token || token->type != word)
+		return (ft_return_error("Syntax error: expected redirect target"));
+	*fd = ft_open(token->value, flags, 0644);
+	ft_remove_token(token);
+	return (0);
 }
