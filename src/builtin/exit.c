dabd1f4a8f6304f6ee89671bfd6404f9395ab15f
Maarten Joosten <mjoosten@student.codam.nl>
2022-04-12 16:05:40 +0200

    Added ft_isvalid

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -1,4 +1,5 @@
 #include "libft.h"
+#include "limits.h"
 
 void	ft_exit(char *str)
 {
@@ -7,4 +8,29 @@ void	ft_exit(char *str)
 	if (str)
 		exit(ft_atoi(str));
 	exit(EXIT_SUCCESS);
+}
+
+int		ft_isvalid(char *str)
+{
+	int64_t		number;
+	int			min;
+
+	min = 0;
+	while (*str == ' ' || *str == '\t')
+		str++;
+	if (!str)
+		return (0);
+	if (*str == '-')
+	{
+		min++;
+		str++;
+	}
+	if (ft_strlen(str) > 19)
+		return (0);
+	number = 0;
+	while (*str)
+		number = 10 * number + *str++ - '0';
+	if (number < LONG_MIN || number > LONG_MAX)
+		return (0);
+	return (1);
 }
