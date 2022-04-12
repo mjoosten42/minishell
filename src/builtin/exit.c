b6d509cd1fcb014adedc1a11fc4851552f7a3ec6
RubenNijhuis <contact@rubennijhuis.com>
2022-04-12 16:07:48 +0200

    feat: exit int check [WIP]

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -1,10 +1,28 @@
 #include "libft.h"
 
-void	ft_exit(char *str)
+int	is_all_digits(char *str)
 {
+	int strlen;
+	int i;
+
+	strlen = ft_strlen(str);
+	i = 0;
+	while (i < strlen)
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	ft_exit(char **strs)
+{
+	if (strs[2] && is_all_digits(strs[2]))
+		
 	if (isatty(STDIN_FILENO))
 		ft_putstr("exit\n");
-	if (str)
-		exit(ft_atoi(str));
+	if (strs[1])
+		exit(ft_atoi(strs[1]));
 	exit(EXIT_SUCCESS);
 }
