9298cebd9353a45e88c4d458eecd22846a0bbd44
Maarten Joosten <mjoosten@student.codam.nl>
2022-04-12 17:00:20 +0200

    exit working possibly

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -1,9 +1,29 @@
 #include "minishell.h"
 #include "libft.h"
-#include "limits.h"
 
-int				ft_isvalid(char *str, unsigned char *number);
-unsigned char ft_char_atoi(char *str);
+int				is_all_digits(char *str);
+unsigned char 	ft_char_atoi(char *str);
+
+void	ft_exit(char **strs)
+{
+	t_program_data	*pd;
+
+	pd = pd_get();
+	if (isatty(STDIN_FILENO))
+		ft_putstr("exit\n");
+	if (!strs[1])
+		exit(WEXITSTATUS(pd->last_exit_status));
+	if (!is_all_digits(strs[1]) || ft_strlen(strs[1]) > 19)
+	{
+		ft_putstr_fd("minishell: exit: ", 2);
+		ft_putstr_fd(strs[1], 2);
+		ft_putendl_fd(": numeric argument required", 2);
+	}
+	else if (strs[2])
+		ft_putendl_fd("minishell: exit: too many arguments", 2);
+	else
+		exit(ft_char_atoi(strs[1]));
+}
 
 int	is_all_digits(char *str)
 {
@@ -23,26 +43,6 @@ int	is_all_digits(char *str)
 	return (1);
 }
 
-void	ft_exit(char **strs)
-{
-	t_program_data	*pd;
-
-	pd = pd_get();
-	if (isatty(STDIN_FILENO))
-		ft_putstr("exit\n");
-	if (!strs[1])
-		exit(WEXITSTATUS(pd->last_exit_status));
-	if (!is_all_digits(strs[1]) || ft_strlen(strs[1]) > 19)
-	{
-		ft_putstr_fd("minishell: exit: ", 2);
-		ft_putstr_fd(strs[1], 2);
-		ft_putendl_fd(": numeric argument required", 2);
-		return ;
-	}
-	if (strs[2])
-		return (ft_putendl_fd("minishell: exit: too many arguments", 2));
-	exit(ft_char_atoi(strs[1]));
-}
 
 unsigned char ft_char_atoi(char *str)
 {
