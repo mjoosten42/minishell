e3e7451adc93c848409e75b76664b2063a785751
Maarten Joosten <mjoosten@student.codam.nl>
2022-04-21 10:38:29 +0200

    removed check for double 8194

--- src/lexer.c
+++ src/lexer.c
@@ -6,7 +6,7 @@
 /*   By: mjoosten <mjoosten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 10:11:32 by mjoosten          #+#    #+#             */
-/*   Updated: 2022/04/21 10:13:35 by mjoosten         ###   ########.fr       */
+/*   Updated: 2022/04/21 10:37:43 by mjoosten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 
 t_token	*special_char_token(char *str);
 t_token	*word_token(char *str);
-int		dollar_len(char *str);
 
 void	ft_lexer(t_token *head, char *str)
 {
@@ -47,7 +46,7 @@ t_token	*special_char_token(char *str)
 		while (str[len] == ' ' || str[len] == '\t')
 			len++;
 	if (type == dollar)
-		len += dollar_len(&str[1]);
+		len += export_name_len(&str[1]);
 	if ((type == red_in && str[1] == '<') || (type == red_out && str[1] == '>'))
 	{
 		len++;
@@ -73,11 +72,4 @@ t_token	*word_token(char *str)
 		i++;
 	token->value = ft_substr(str, 0, i);
 	return (token);
-}
-
-int	dollar_len(char *str)
-{
-	if (*str == '?')
-		return (1);
-	return (export_name_len(str));
 }
